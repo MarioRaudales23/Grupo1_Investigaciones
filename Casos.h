b29378617a269868118f7730d912f4418a236af0
miltonmb <milton.pasos@gmail.com>
2016-09-09 14:37:05 -0600

    Clase homicidio creada completamente

--- Casos.h
+++ Casos.h
@@ -4,21 +4,24 @@
 using namespace std;
 class Casos{
 	private:
+		int numero;
 		vector<evidencias> evidencias;
 		string hora;
 		string fecha;
 		bool cerrado;
 
 	public:
-		Casos(string,string,bool);
-		virtual ~Casos()=0;
+		Casos(string,string,bool,int);
+		virtual ~Casos();
 		string getFecha();
 		string getHora();
 		bool getCerrado();
 		void setFecha();
 		void setHora();
 		void setCerrado();
-		void toString()=0;
+		int getNumero();
+		void setNumero();
+		virtual string toString()const;
 		void setEvidencia(evidencias);
 
 }
