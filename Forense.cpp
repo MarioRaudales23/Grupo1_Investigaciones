47637da4599a6a0bca29bc494d9d81a203432ff4
Ana Gomez <anagomez98@unitec.edu>
2016-09-09 15:31:26 -0600

    :(

--- Forense.cpp
+++ Forense.cpp
@@ -7,18 +7,47 @@
 using std::string;
 using std::stringstream;
 
+
 Forense::Forense() : Personas::Personas(){
 
 
 }
+
 
 Forense::Forense(string nombreReal, string usuario, string password, int edad, int ID, string fechaNacimiento, string fechaIngreso, string horario) : Personas(nombreReal, usuario, password, edad, ID, fechaNacimiento), fechaIngreso(fechaIngreso), horario(horario) {
 
 
 }
 
+
 Forense::~Forense() {
 
 
 }
 
+
+string Forense::getFechaIngreso() {
+
+	return this -> fechaIngreso;
+
+}
+
+void Forense::setFechaIngreso(string fechaIngreso) {
+
+	this -> fechaIngreso = fechaIngreso;
+}
+
+string Forense::getHorario() {
+	return this -> horario;
+
+}
+
+void Forense::setHorario(string horario) {
+
+	this -> horario = horario;
+
+}
+
+string Forense::toString() {
+	
+}
